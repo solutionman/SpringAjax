bf51ea802439298674e81773b4a7b1b0415fe3a2
lom <diskedge@yandex.ru>
2020-02-11 18:14:22 +0700

    SearchController

--- src/main/java/com/ajax/controller/SearchController.java
+++ src/main/java/com/ajax/controller/SearchController.java
@@ -1,5 +1,35 @@
 package com.ajax.controller;
 
+import com.ajax.model.AjaxResponseBody;
+import com.ajax.model.SearchCriteria;
+import com.ajax.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
 public class SearchController {
 
+    UserService userService;
+
+    @Autowired
+    public void setUserService(UserService userService){
+        this.userService = userService;
+    }
+
+    @PostMapping("/api/search")
+    public ResponseEntity<?> getSearchResultVaAjax(@Valid @RequestBody SearchCriteria search, Errors errors){
+
+        AjaxResponseBody result = new AjaxResponseBody();
+
+
+        return ResponseEntity.ok(result);
+    }
 }
