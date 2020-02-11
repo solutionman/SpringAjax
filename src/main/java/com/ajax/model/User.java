1559914258b36c2065b8a5f90f1cf7dbeca92cc4
lom <diskedge@yandex.ru>
2020-02-11 18:35:58 +0700

    User

--- src/main/java/com/ajax/model/User.java
+++ src/main/java/com/ajax/model/User.java
@@ -1,4 +1,16 @@
 package com.ajax.model;
 
+import lombok.Data;
+
+@Data
 public class User {
+    String username;
+    String password;
+    String email;
+
+    public User(String username, String password, String email){
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
 }
