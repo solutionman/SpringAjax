af06c85b98144e08ae26fd4be1dcf8ef79efbf09
lom <diskedge@yandex.ru>
2020-02-11 19:22:03 +0700

    SpringBootWebApplication

--- src/main/java/com/ajax/SpringBootWebApplication.java
+++ src/main/java/com/ajax/SpringBootWebApplication.java
@@ -1,4 +1,11 @@
 package com.ajax;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class SpringBootWebApplication {
+    public static void main(String[] args){
+        SpringApplication.run(SpringBootApplication.class, args);
+    }
 }
